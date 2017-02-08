6f36f1748b2bf74db112a060d78bbcb6d191beca
Brynna McCarter and Keith Evans <brynna@>
2017-02-08 15:41:23 -0800

    perfect tamagotchi app

--- README.md
+++ README.md
@@ -30,18 +30,18 @@ _Make sure to have composer installed before running this app._
 
 ## Known Bugs
 
-_{Are there issues that have not yet been resolved that you want to let users know you know?  Outline any issues that would impact use of your application.  Share any workarounds that are in place. }_
+_No known bugs_
 
 ## Support and contact details
 
-_{Let people know what to do if they run into any issues or have questions, ideas or concerns.  Encourage them to contact you or make a contribution to the code.}_
+_Any questions, comments, or bug reports can be directed to the repository administrator._
 
 ## Technologies Used
 
-_{Tell me about the languages and tools you used to create this app. Assume that I know you probably used HTML and CSS. If you did something really cool using only HTML, point that out.}_
+_PHP/Silex/Twig/Bootstrap/jQuery_
 
 ### License
 
-*{Determine the license under which this application can be used.  See below for more details on licensing.}*
+*This app is under the MIT license.*
 
-Copyright (c) 2016 **_{List of contributors or company name}_**
+Copyright (c) 2017 **_Brynna Klamkin-McCarter and Keith Evans_**
--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,51 +1,72 @@
-// 1. timer/incrementation on PHP end
-// 2. AJAX request GET to get values from PHP
-// 3. update front end on request return
-
 $(function() {
-  var interval;
-  function update() {
-    $.get("/increment", function(response) {
-      if (response.match(/.*(dead)+/i)) {
-        clearInterval(interval);
-        $("#tamagotchi-gif").attr("src", "image/dead.gif");
-      }
-      $("div.card-block").html(response);
-    });
-  }
+  var pageStart = setTimeout(function() {
+    var interval;
 
-  $("#feed-button").click(function() {
-    $.get("/feed", function(response) {
-      $("#feed-form").html(response);
-      temporaryGif("feed");
-    });
-  });
-
-  $("#nap-button").click(function() {
-    $.get("/nap", function(response) {
-      $("#nap-form").html(response);
-      temporaryGif("nap");
-    });
-  });
-
-  $("#play-button").click(function() {
-    $.get("/play", function(response) {
-      $("#play-form").html(response);
-      temporaryGif("good-dance");
-    });
-  });
-
-  function temporaryGif(gifName) {
-    $("#tamagotchi-gif").attr("src", "image/" + gifName + ".gif");
-    $("#buttons-div").children().prop("disabled", true);
-
-    var timeout = setTimeout(function() {resetInterface()}, 3000);
-  }
-
-  function resetInterface() {
     $("#tamagotchi-gif").attr("src", "image/dance.gif");
     $("#buttons-div").children().prop("disabled", false);
-  }
 
-  interval = setInterval(function() {update()}, 1000);
+    function update() {
+      $.get("/increment", function(response) {
+        if (response.match(/.*(dead)+/i)) {
+          clearInterval(interval);
+          $("#tamagotchi-gif").attr("src", "image/dead.gif");
+        }
+        $("div.card-block").html(response);
+
+        if ($("#bar1").attr("class").match(/bg-(\w+)/i)) {
+          var feedColor = $("#bar1").attr("class").match(/bg-(\w+)/i)[1];
+        } else {
+          var feedColor = "primary";
+        }
+        if ($("#bar2").attr("class").match(/bg-(\w+)/i)) {
+          var napColor = $("#bar2").attr("class").match(/bg-(\w+)/i)[1];
+        } else {
+          var napColor = "primary";
+        }
+        if ($("#bar3").attr("class").match(/bg-(\w+)/i)) {
+          var playColor = $("#bar3").attr("class").match(/bg-(\w+)/i)[1];
+        } else {
+          var playColor = "primary";
+        }
+        $("#feed-button").removeClass().addClass("bg-" + feedColor + " btn");
+        $("#nap-button").removeClass().addClass("bg-" + napColor + " btn");
+        $("#play-button").removeClass().addClass("bg-" + playColor + " btn");
+      });
+    }
+
+    $("#feed-button").click(function() {
+      $.get("/feed", function(response) {
+        $("#feed-form").html(response);
+        temporaryGif("feed");
+      });
+    });
+
+    $("#nap-button").click(function() {
+      $.get("/nap", function(response) {
+        $("#nap-form").html(response);
+        temporaryGif("nap");
+      });
+    });
+
+    $("#play-button").click(function() {
+      $.get("/play", function(response) {
+        $("#play-form").html(response);
+        temporaryGif("good-dance");
+      });
+    });
+
+    function temporaryGif(gifName) {
+      $("#tamagotchi-gif").attr("src", "image/" + gifName + ".gif");
+      $("#buttons-div").children().prop("disabled", true);
+
+      var timeout = setTimeout(function() {resetInterface()}, 3000);
+    }
+
+    function resetInterface() {
+      $("#tamagotchi-gif").attr("src", "image/dance.gif");
+      $("#buttons-div").children().prop("disabled", false);
+    }
+
+    interval = setInterval(function() {update()}, 1000);
+  }, 14500);
 });
